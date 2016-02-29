92de3a6c7ede45b9d4ff8d0d61e3a55af751b4b6
Steve McCarthy <steve@redlua.com>
2016-02-28 19:50:19 -0500

    Remove specific Millisecond reference, more prep for different time resolutions

--- examples/one-second.go
+++ examples/one-second.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	w := sanic.NewWorker(0, 1288834974657, 4, 14, 41)
+	w := sanic.NewWorker(0, 1288834974657, 4, 14, 41, time.Millisecond)
 
 	start := time.Now()
 
--- worker.go
+++ worker.go
@@ -14,44 +14,47 @@ type Worker struct {
 	LastTimeStamp  int64
 	TimeStampBits  uint64
 	TimeStampShift uint64
+	Frequency      time.Duration
 	TotalBits      uint64
 	CustomEpoch    int64
 }
 
 func NewWorker(
-	id, epoch int64,
-	idBits, sequenceBits, timestampBits uint64) Worker {
+	id, epoch int64, idBits, sequenceBits, timestampBits uint64,
+	frequency time.Duration) Worker {
 
 	totalBits := idBits + sequenceBits + timestampBits + 1
 	if totalBits%6 != 0 {
 		log.Fatal("totalBits + 1 must be evenly divisible by 6")
 	}
 
-	return Worker{
+	w := Worker{
 		ID:             id,
 		IDBits:         idBits,
 		IDShift:        sequenceBits,
 		Sequence:       0,
 		SequenceBits:   sequenceBits,
-		LastTimeStamp:  TimeMillis(),
 		TimeStampBits:  timestampBits,
 		TimeStampShift: sequenceBits + idBits,
+		Frequency:      frequency,
 		TotalBits:      totalBits,
 		CustomEpoch:    epoch,
 	}
+	w.LastTimeStamp = w.Time()
+	return w
 }
 
 func (w *Worker) NextID() int64 {
-	timestamp := TimeMillis()
+	timestamp := w.Time()
 
 	if w.LastTimeStamp > timestamp {
-		w.WaitForNextMilli()
+		w.WaitForNextTime()
 	}
 
 	if w.LastTimeStamp == timestamp {
 		w.Sequence = (w.Sequence + 1) % (1 << w.SequenceBits)
 		if w.Sequence == 0 {
-			w.WaitForNextMilli()
+			w.WaitForNextTime()
 			timestamp = w.LastTimeStamp
 		}
 	} else {
@@ -70,14 +73,14 @@ func (w *Worker) IDString(id int64) string {
 	return str
 }
 
-func (w *Worker) WaitForNextMilli() {
-	ts := TimeMillis()
+func (w *Worker) WaitForNextTime() {
+	ts := w.Time()
 	for ts <= w.LastTimeStamp {
-		ts = TimeMillis()
+		ts = w.Time()
 	}
 	w.LastTimeStamp = ts
 }
 
-func TimeMillis() int64 {
-	return time.Now().UnixNano() / int64(time.Millisecond)
+func (w *Worker) Time() int64 {
+	return time.Now().UnixNano() / int64(w.Frequency)
 }
